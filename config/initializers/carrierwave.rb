f5c884819501abc688bb3758b6caaddc9d71bf4d
Wendy Cordero <wcconsulting11@gmail.com>
2019-08-27 00:42:46 +0000

    uncomment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,9 @@
 # config/initializers/carrierwave.rb
-
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
   config.fog_provider = 'fog/aws'                        # required
-  #config.fog_public = false
+  config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
